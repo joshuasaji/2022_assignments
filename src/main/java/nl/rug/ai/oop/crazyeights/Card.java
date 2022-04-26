c4f1a82f09c0814d1d451ce9ac8121ef2938f0b8
Joshua Chacko <j.saji.chacko@student.rug.nl>
2022-04-26 11:18:06 +0200

    All hail Harmen

--- src/main/java/nl/rug/ai/oop/crazyeights/Card.java
+++ src/main/java/nl/rug/ai/oop/crazyeights/Card.java
@@ -4,4 +4,9 @@ package nl.rug.ai.oop.crazyeights;
  * Represents a playing card
  */
 public class Card {
+    public static final String[]  rank = {
+        null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen"};
+    public static final String[]  SUITS = {
+            "Hearts", "Diamonds", "Spades", "Clubs"};
+
 }
